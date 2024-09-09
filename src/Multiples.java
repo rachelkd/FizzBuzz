a74018f97bf13d928a90322286e3d4820da40cbb
rachelkd <rachel05deng@gmail.com>
2024-09-09 16:24:23 -0400

    Implemented Multiples class

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    public static void main(String[] args) {
+        int multiplesSoFar = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean multipleOfThree = i % 3 == 0;
+            boolean multipleOfFive = i % 5 == 0;
+            if (multipleOfThree || multipleOfFive) {
+                multiplesSoFar++;
+            }
+        }
+        System.out.println(multiplesSoFar);
+    }
 }
